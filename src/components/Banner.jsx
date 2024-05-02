7c6013722e1be3b6650f86bf5a418232ff0f7d54
Sebastian <sebassuarez103@gmail.com>
2024-05-02 15:06:47 -0500

    Intefaz inicial

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <section class="banner">
+            
+        </section>
+    </div>
+  )
+}
+
+export default Banner
+
