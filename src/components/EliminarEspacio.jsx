eb0b99e991ab01508ce93fc0aa5cf95a35c1e2d9
Sebastian <sebassuarez103@gmail.com>
2024-06-07 10:38:50 -0500

    configuraciones en eliminar espacio

--- src/components/EliminarEspacio.jsx
+++ src/components/EliminarEspacio.jsx
@@ -6,6 +6,8 @@ const EliminarEspacio = ({ onClose }) => {
     const [espacioId, setEspacioId] = useState('');
     const [error, setError] = useState('');
     const [mostrarConfirmacion, setMostrarConfirmacion] = useState(false);
+    const [confirmacionTexto, setConfirmacionTexto] = useState('');
+    const [eliminado, setEliminado] = useState(false);
 
     // Cargar todos los espacios deportivos al montar el componente
     useEffect(() => {
@@ -21,16 +23,25 @@ const EliminarEspacio = ({ onClose }) => {
         cargarEspacios();
     }, []);
 
-    const handleEliminar = async () => {
+    const handleEliminar = () => {
         if (!espacioId) {
             setError('Seleccione un espacio para eliminar');
+            return;
+        }
+        setMostrarConfirmacion(true);
+    };
+
+    const confirmarEliminacion = async () => {
+        if (confirmacionTexto !== 'confirmar') {
+            setError('Debes escribir "confirmar" para eliminar el espacio deportivo');
             return;
         }
         try {
             await axios.delete(`http://localhost:9090/api/v1/tipos-espacios-deportivos/${espacioId}`);
             setEspacios(espacios.filter(espacio => espacio.id !== parseInt(espacioId)));
-            setMostrarConfirmacion(true);
+            setMostrarConfirmacion(false);
             setError('');
+            setEliminado(true);
         } catch (error) {
             setError('Error al eliminar el espacio');
             console.error('Error al eliminar espacio:', error);
@@ -41,10 +52,25 @@ const EliminarEspacio = ({ onClose }) => {
         <div className="formulario-crear-espacio">
             <h2 className="titulo_crearespacio">Eliminar Espacio Deportivo</h2>
             {error && <p className="error">{error}</p>}
-            {mostrarConfirmacion ? (
+            {eliminado ? (
                 <div className="mensaje-confirmacion">
                     <p>¡El espacio deportivo fue eliminado exitosamente!</p>
                     <button onClick={onClose} className="boton-guardar">Regresar a Espacios</button>
+                </div>
+            ) : mostrarConfirmacion ? (
+                <div className="mensaje-confirmacion">
+                    <p>¿Estás seguro?</p> 
+                    <p>Escribe aquí "confirmar" para eliminar el tipo de espacio deportivo definitivamente.</p>
+                    <input
+                        type="text"
+                        value={confirmacionTexto}
+                        onChange={(e) => setConfirmacionTexto(e.target.value)}
+                        placeholder="Escribe 'confirmar' aquí"
+                    />
+                    <div className="button-container">
+                        <button onClick={confirmarEliminacion} className='boton-guardar'>Confirmar Eliminación</button>
+                        <button onClick={() => setMostrarConfirmacion(false)} className='boton-cancelar'>Cancelar</button>
+                    </div>
                 </div>
             ) : (
                 <>
